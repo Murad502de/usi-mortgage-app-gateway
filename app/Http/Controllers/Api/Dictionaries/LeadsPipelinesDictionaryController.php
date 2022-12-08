cf2d6a5c6961a6ae2ccfbd806d52c1d6c53264a5
itWelt <rassulovmurad@gmail.com>
2022-12-08 23:25:02 +0300

    added LeadsPipelinesDictionaryController prototype

---
+++ app/Http/Controllers/Api/Dictionaries/LeadsPipelinesDictionaryController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Dictionaries;
+
+use App\Http\Controllers\Controller;
+
+class LeadsPipelinesDictionaryController extends Controller
+{
+    public function pipelines()
+    {}
+}
