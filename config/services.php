541434fc8ab3a100eff34652292f495661df8329
itWelt <rassulovmurad@gmail.com>
2022-12-08 23:06:53 +0300

    edit config/services

--- config/services.php
+++ config/services.php
@@ -1,33 +1,24 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+    'mailgun'  => [
+        'domain'   => env('MAILGUN_DOMAIN'),
+        'secret'   => env('MAILGUN_SECRET'),
         'endpoint' => env('MAILGUN_ENDPOINT', 'api.mailgun.net'),
     ],
-
     'postmark' => [
         'token' => env('POSTMARK_TOKEN'),
     ],
-
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
+    'ses'      => [
+        'key'    => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
-
+    'amoCRM'   => [
+        'client_secret'       => env('AMOCRM_CLIENT_SECRET', null),
+        'redirect_uri'        => env('AMOCRM_REDIRECT_URI', null),
+        'subdomain'           => env('AMOCRM_SUBDOMAIN', null),
+        'successful_stage_id' => env('AMOCRM_SUCCESSFUL_STAGE_ID', null),
+        'loss_stage_id'       => env('AMOCRM_LOSS_STAGE_ID', null),
+    ],
 ];
