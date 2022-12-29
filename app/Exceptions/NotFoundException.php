51defd607a5802c76092ece53b9c11e5fb2ff3e9
itWelt <rassulovmurad@gmail.com>
2022-12-30 00:39:52 +0300

    [main][task] added NotFoundException

---
+++ app/Exceptions/NotFoundException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Illuminate\Http\Response;
+
+class NotFoundException extends Exception
+{
+    /**
+     * Render the exception as an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request)
+    {
+        return response()->json([
+            'message' => $this->getMessage(),
+        ], Response::HTTP_NOT_FOUND);
+    }
+}
