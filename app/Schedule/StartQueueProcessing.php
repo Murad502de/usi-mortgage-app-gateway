23491357c548ccdb19ca072a52271d1cd8db859c
itWelt <rassulovmurad@gmail.com>
2023-01-03 15:56:08 +0300

    [main][task] edit kernel of scheduler

--- app/Schedule/StartQueueProcessing.php
+++ app/Schedule/StartQueueProcessing.php
@@ -5,7 +5,7 @@ namespace App\Schedule;
 class StartQueueProcessing
 {
     private $localCommand = "cd /var/www/html/ && php artisan queue:work --stop-when-empty";
-    private $command      = "cd ~/www/hub.integrat.pro/Murad/x-mara-discount-cards && /opt/php/7.3/bin/php artisan queue:work --stop-when-empty";
+    private $command      = "cd ~/www/hub.integrat.pro/Murad/usi-mortgage-app-gateway && /opt/php/7.3/bin/php artisan queue:work --stop-when-empty";
 
     public function __invoke(bool $isLocal = false): string
     {
