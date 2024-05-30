b50fcfc106d6e97a3dd7807b207094b625595cd8
itWelt <rassulovmurad@gmail.com>
2024-05-30 23:27:53 +0300

    [main][]

--- app/Models/Lead.php
+++ app/Models/Lead.php
@@ -67,7 +67,8 @@ class Lead extends Model
 
             if (!!$mortgageLead && !!$mortgageLead->is_mortgage) {
                 dump('Mortgage Lead is found'); //DELETE
-                return self::mortgageExist($mortgageLead);
+                $amoMortgageLead = self::fetchLeadById($mortgageLead->amo_id);
+                return self::mortgageExist($amoMortgageLead);
             }
 
             return null;
@@ -268,23 +269,10 @@ class Lead extends Model
             (int) config('services.amoCRM.exclude_cf_lead_manager_id'),
         ];
     }
-    // public static function mortgageExist(array $mortgageLead): bool
-    // {
-    //     Log::info(__METHOD__, [$mortgageLead]); //DELETE
-
-    //     self::$AMO_API->createTask(
-    //         (int) $mortgageLead['responsible_user_id'],
-    //         (int) $mortgageLead['id'],
-    //         time() + 3600 * 3,
-    //         'Менеджер повторно отправил запрос на ипотеку',
-    //         self::$TASK_TYPE_CONTROLL_ID
-    //     );
-
-    //     return true;
-    // }
-    public static function mortgageExist($mortgageLead): bool
+    public static function mortgageExist(array $mortgageLead): bool
     {
         dump($mortgageLead); //DELETE
+        // Log::info(__METHOD__, [$mortgageLead]); //DELETE
 
         // self::$AMO_API->createTask(
         //     (int) $mortgageLead['responsible_user_id'],
